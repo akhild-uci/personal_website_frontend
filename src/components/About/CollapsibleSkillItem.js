7c36b08f09938805540f1105bd670f37d95d0cd4
akhild-uci <akhil.dhanala@gmail.com>
2020-12-26 16:18:57 -0800

    added expansion awareness

--- src/components/About/CollapsibleSkillItem.js
+++ src/components/About/CollapsibleSkillItem.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import PropType from 'prop-types';
 import Subskills from './Subskills';
 import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
 import Accordion from 'react-bootstrap/Accordion';
 
 
 class CollapsibleSkillItem extends Component {
+    state = {
+        isExpanded: false
+    }
+
+    handleClick = () => {
+        this.setState({isExpanded : !this.state.isExpanded})
+    }
     render() {
         const { name, subskills } = this.props.skill;
         return (
             <Card>
-                <Accordion.Toggle as={Card.Header} eventKey={name}>
+                <Accordion.Toggle as={Card.Header} eventKey={name} onClick={this.handleClick}>
                     {name}
+                    <Badge variant="secondary" style={badgeStyle}>
+                        {this.state.isExpanded ? "-" : "+"}
+                    </Badge>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={name}>
                     <Subskills subskills={subskills}/>
@@ -20,6 +31,9 @@ class CollapsibleSkillItem extends Component {
         )
     }
 }
+
+const badgeStyle = { float: "right"}
+
 
 
 // PropTypes
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import Skills from './Skills';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import profilePic from '../media/profile_pic.jpg';
 
 
 function About() {
     return (
         <Jumbotron id="about" fluid>
-            <h2>About Me</h2>
-            <div className="skills-container">
-                <Skills />
-            </div>
+            <Container>
+                <Row className="justify-content-center">
+                    <h2>About Me</h2>
+                </Row>
+                <br/>
+                <Row>
+                    <Col>
+                        <Image src={profilePic} style={imgStyle} roundedCircle/>
+                    </Col>
+                    <Col>
+                        <div className="skills-container">
+                            <h3 style={{textAlign: "center"}}>Skills</h3>
+                            <Skills />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </Jumbotron>
     )
+}
+
+const imgStyle = {
+    height: "143.5px",
+    width: "132px"
 }
 
 
