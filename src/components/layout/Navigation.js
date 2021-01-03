42db579bd3f1bbc7c6dc87acaa3a5a7d4237aa0d
akhild-uci <akhil.dhanala@gmail.com>
2021-01-02 20:39:17 -0800

    dynamic navigation

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -3,13 +3,40 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 class Navigation extends Component {
+    state = {
+        background: "transparent",
+        color : "white"
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.scrollListener);
+    }
+
+    scrollListener = () => {
+        let pos = window.scrollY;
+        let home = document.getElementById('home').clientHeight;
+        if (pos >= home) {
+            this.setState({background : "dark", color : "white"});
+        } else if (pos <= (home / 10)) {
+            this.setState({background : "transparent", color : "white"});
+        } else {
+            this.setState({background : "transparent", color : "hide"});
+        }
+    }
+
     render() {
         return (
-            <Navbar bg="dark" variant="dark" fixed="top" className="justify-content-center">
-                <Nav.Link href="#home" className="nav-link">Home</Nav.Link>
-                <Nav.Link href="#about" className="nav-link">About</Nav.Link>
-                <Nav.Link href="#projects" className="nav-link">Projects</Nav.Link>
-                <Nav.Link href="#contact" className="nav-link">Contact</Nav.Link>
+            <Navbar
+                bg={this.state.background}
+                variant="dark"
+                fixed="top"
+                className="justify-content-center"
+                style={{transition : "all .3s ease-in-out"}}
+            >
+                <Nav.Link className={this.state.color} href="#home">Home</Nav.Link>
+                <Nav.Link className={this.state.color} href="#about">About</Nav.Link>
+                <Nav.Link className={this.state.color} href="#projects">Projects</Nav.Link>
+                <Nav.Link className={this.state.color} href="#contact">Contact</Nav.Link>
             </Navbar>
         )
     }
